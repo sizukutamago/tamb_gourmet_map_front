36cc52ecca29d10cc7eec57927e01b3f5636c72e
sizukutamago <tanaka@tam-bourine.co.jp>
2019-12-30 19:18:34 +0900

    delete logo

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
