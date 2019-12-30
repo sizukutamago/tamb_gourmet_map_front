2a950e80af7a4c37eeab136e429779e68d0e232a
sizukutamago <tanaka@tam-bourine.co.jp>
2019-12-30 20:06:51 +0900

    google map表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import GoogleMapReact from 'google-map-react';
 import './App.css';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_KEY }}
+        defaultCenter={{ lat: 35.695491, lng: 139.763253 }}
+        defaultZoom={17}
+      />
     </div>
   );
 }
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="react-scripts" />
+
+declare namespace NodeJS {
+    interface ProcessEnv {
+        REACT_APP_GOOGLE_MAP_KEY: string
+    }
+}
